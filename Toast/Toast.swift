c4d4446b590fe466820b4fb5e68175c83f0c69d3
Anton Aleshkevich <a.aleshkevich@tehnoliga.ru>
2015-12-10 00:43:54 +0300

    Additional Access Control fix for ToastStyle and ToastManager

--- Toast/Toast.swift
+++ Toast/Toast.swift
@@ -582,26 +582,30 @@ public extension UIView {
 */
 public struct ToastStyle {
     
+    public init() {
+        // allow creating style object
+    }
+
     /**
      The background color. Default is `UIColor.blackColor()` at 80% opacity.
     */
-    var backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.8)
+    public var backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.8)
     
     /**
      The title color. Default is `UIColor.whiteColor()`.
     */
-    var titleColor = UIColor.whiteColor()
+    public var titleColor = UIColor.whiteColor()
     
     /**
      The message color. Default is `UIColor.whiteColor()`.
     */
-    var messageColor = UIColor.whiteColor()
+    public var messageColor = UIColor.whiteColor()
     
     /**
      A percentage value from 0.0 to 1.0, representing the maximum width of the toast
      view relative to it's superview. Default is 0.8 (80% of the superview's width).
     */
-    var maxWidthPercentage: CGFloat = 0.8 {
+    public var maxWidthPercentage: CGFloat = 0.8 {
         didSet {
             maxWidthPercentage = max(min(maxWidthPercentage, 1.0), 0.0)
         }
@@ -611,7 +615,7 @@ public struct ToastStyle {
      A percentage value from 0.0 to 1.0, representing the maximum height of the toast
      view relative to it's superview. Default is 0.8 (80% of the superview's height).
     */
-    var maxHeightPercentage: CGFloat = 0.8 {
+    public var maxHeightPercentage: CGFloat = 0.8 {
         didSet {
             maxHeightPercentage = max(min(maxHeightPercentage, 1.0), 0.0)
         }
@@ -622,65 +626,65 @@ public struct ToastStyle {
      is present, this is also used as the padding between the image and the text.
      Default is 10.0.
     */
-    var horizontalPadding: CGFloat = 10.0
+    public var horizontalPadding: CGFloat = 10.0
     
     /**
      The spacing from the vertical edge of the toast view to the content. When a title
      is present, this is also used as the padding between the title and the message.
      Default is 10.0.
     */
-    var verticalPadding: CGFloat = 10.0
+    public var verticalPadding: CGFloat = 10.0
     
     /**
      The corner radius. Default is 10.0.
     */
-    var cornerRadius: CGFloat = 10.0;
+    public var cornerRadius: CGFloat = 10.0;
     
     /**
      The title font. Default is `UIFont.boldSystemFontOfSize(16.0)`.
     */
-    var titleFont = UIFont.boldSystemFontOfSize(16.0)
+    public var titleFont = UIFont.boldSystemFontOfSize(16.0)
     
     /**
      The message font. Default is `UIFont.systemFontOfSize(16.0)`.
     */
-    var messageFont = UIFont.systemFontOfSize(16.0)
+    public var messageFont = UIFont.systemFontOfSize(16.0)
     
     /**
      The title text alignment. Default is `NSTextAlignment.Left`.
     */
-    var titleAlignment = NSTextAlignment.Left
+    public var titleAlignment = NSTextAlignment.Left
     
     /**
      The message text alignment. Default is `NSTextAlignment.Left`.
     */
-    var messageAlignment = NSTextAlignment.Left
+    public var messageAlignment = NSTextAlignment.Left
     
     /**
      The maximum number of lines for the title. The default is 0 (no limit).
     */
-    var titleNumberOfLines = 0;
+    public var titleNumberOfLines = 0;
     
     /**
      The maximum number of lines for the message. The default is 0 (no limit).
     */
-    var messageNumberOfLines = 0;
+    public var messageNumberOfLines = 0;
     
     /**
      Enable or disable a shadow on the toast view. Default is `false`.
     */
-    var displayShadow = false;
+    public var displayShadow = false;
     
     /**
      The shadow color. Default is `UIColor.blackColor()`.
      */
-    var shadowColor = UIColor.blackColor()
+    public var shadowColor = UIColor.blackColor()
     
     /**
      A value from 0.0 to 1.0, representing the opacity of the shadow.
      Default is 0.8 (80% opacity).
     */
-    var shadowOpacity: Float = 0.8 {
+    public var shadowOpacity: Float = 0.8 {
         didSet {
             shadowOpacity = max(min(shadowOpacity, 1.0), 0.0)
         }
@@ -689,28 +693,28 @@ public struct ToastStyle {
     /**
      The shadow radius. Default is 6.0.
     */
-    var shadowRadius: CGFloat = 6.0
+    public var shadowRadius: CGFloat = 6.0
     
     /**
      The shadow offset. The default is 4 x 4.
     */
-    var shadowOffset = CGSize(width: 4.0, height: 4.0)
+    public var shadowOffset = CGSize(width: 4.0, height: 4.0)
     
     /**
      The image size. The default is 80 x 80.
     */
-    var imageSize = CGSize(width: 80.0, height: 80.0)
+    public var imageSize = CGSize(width: 80.0, height: 80.0)
     
     /**
      The size of the toast activity view when `makeToastActivity(position:)` is called.
      Default is 100 x 100.
     */
-    var activitySize = CGSize(width: 100.0, height: 100.0)
+    public var activitySize = CGSize(width: 100.0, height: 100.0)
     
     /**
      The fade in/out animation duration. Default is 0.2.
      */
-    var fadeDuration: NSTimeInterval = 0.2
+    public var fadeDuration: NSTimeInterval = 0.2
     
 }
 
@@ -725,18 +729,18 @@ public class ToastManager {
     /**
      The `ToastManager` singleton instance.
      */
-    static let shared = ToastManager()
+    public static let shared = ToastManager()
     
     /**
      The shared style. Used whenever toastViewForMessage(message:title:image:style:) is called
      with with a nil style.
      */
-    var style = ToastStyle()
+    public var style = ToastStyle()
     
     /**
      Enables or disables tap to dismiss on toast views. Default is `true`.
      */
-    var tapToDismissEnabled = true
+    public var tapToDismissEnabled = true
     
     /**
      Enables or disables queueing behavior for toast views. When `true`,
@@ -745,20 +749,20 @@ public class ToastManager {
      on their positions). This has no effect on the toast activity view,
      which operates independently of normal toast views. Default is `true`.
      */
-    var queueEnabled = true
+    public var queueEnabled = true
     
     /**
      The default duration. Used for the `makeToast` and
      `showToast` methods that don't require an explicit duration.
      Default is 3.0.
      */
-    var duration: NSTimeInterval = 3.0
+    public var duration: NSTimeInterval = 3.0
     
     /**
      Sets the default position. Used for the `makeToast` and
      `showToast` methods that don't require an explicit position.
      Default is `ToastPosition.Bottom`.
      */
-    var position = ToastPosition.Bottom
+    public var position = ToastPosition.Bottom
     
 }
