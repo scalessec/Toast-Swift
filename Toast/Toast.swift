40a51c41ae6a55031399d25d2a134f32f2e42955
Bernd Braßel <doc@recordbay.de>
2017-10-16 12:14:30 +0200

    Fixing a bug with making activity toasts
    Activity toasts do not work since commit 2ac97b4dcd738790b875d03f1cc754b87d1f5891.
    This commit fixes the bug.

--- Toast/Toast.swift
+++ Toast/Toast.swift
@@ -198,7 +198,7 @@ public extension UIView {
      */
     public func makeToastActivity(_ position: ToastPosition) {
         // sanity
-        guard let _ = objc_getAssociatedObject(self, &ToastKeys.activityView) as? UIView else { return }
+        guard nil == objc_getAssociatedObject(self, &ToastKeys.activityView) as? UIView else { return }
         
         let toast = createToastActivityView()
         let point = position.centerPoint(forToast: toast, inSuperview: self)
@@ -219,7 +219,7 @@ public extension UIView {
      */
     public func makeToastActivity(_ point: CGPoint) {
         // sanity
-        guard let _ = objc_getAssociatedObject(self, &ToastKeys.activityView) as? UIView else { return }
+        guard nil == objc_getAssociatedObject(self, &ToastKeys.activityView) as? UIView else { return }
         
         let toast = createToastActivityView()
         makeToastActivity(toast, point: point)
