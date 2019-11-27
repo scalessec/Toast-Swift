8a5eecfbc4d61dd8427c7acefa4d3d589e1074ae
Erik Terwan <erik@erikterwan.com>
2019-11-27 22:58:21 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Toast",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "Toast",
+            targets: ["Toast"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Toast",
+            dependencies: [],
+            path: "Toast"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
